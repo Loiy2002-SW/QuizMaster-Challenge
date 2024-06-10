3aefb7aca1ebd71c23328d0c98865595963036a1
loiy <lloiy9310@gmail.com>
2024-06-10 10:53:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizMaster_Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
